eab3ba1273b415f25523f3e521cb4ef072ae4bbc
Zandile <2562617@students.wits.ac.za>
2023-05-19 14:15:12 +0200

    AdventureCard script

---
+++ WSOA2024_Group 19_Final Assignment/Assets/Scripts/Cards/AdventureCard.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player: MonoBehaviour
+{ /*
+    public string Name { get; set; }
+
+    public string Object { get; set; }
+
+
+
+    public Player(string name)
+
+    {
+
+        Name = name;
+
+    }
+
+}
+
+
+
+public class Program
+
+{
+
+    public static void Main()
+
+    {
+
+        List<Player> players = new List<Player>
+
+        {
+
+            new Player("Player 1"),
+
+            new Player("Player 2"),
+
+            new Player("Player 3"),
+
+            new Player("Player 4")
+
+        };
+
+
+
+        string[] adventure_cards = { "Diver", "Pilot", "Engineer", "Explorer",
+
+   "Messenger", "Navigator" };
+
+
+
+        AssignAdventure_CardsToPlayers(players, adventure_cards);
+
+
+
+        foreach (Player player in players)
+
+        {
+
+            Debug.Log(player.Name + " received: " +
+
+                                              player.Adventure_Card);
+
+        }
+
+    }
+
+    private static void AssignAdventure_CardsToPlayers(List<Player> players,
+
+    string[] adventure_cards)
+
+    {
+
+        Random random = new Random();
+
+
+
+        foreach (Player player in players)
+
+        {
+
+            int index = random.Next(adventure_card.Length);
+
+            Player.Adventure_Card = adventure_cards[index];
+
+        }
+
+    } */
+
+}
