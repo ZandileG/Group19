3356abea1e0d8504e4f581511a39a601defbe6c8
Zandile <2562617@students.wits.ac.za>
2023-06-12 18:52:14 +0200

    Revert "Change"
    This reverts commit fa3a13699a396be5ee7247f82b06f9b89926a5fa.

---
+++ WSOA2024_Group 19_Final Assignment/Assets/Scripts/Cards/FloodCardType.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CardType : MonoBehaviour
+{
+    
+    public enum FloodCards
+    {
+        BreakersBridge,
+        BronzeGate,
+        CaveOfShadows,
+        CaveOfEmbers,
+        CliffsOfAbandon,
+        CopperGate,
+        CoralPalace,
+        CrimsonForest,
+        DunesOfDeception,
+        FoolsLanding,
+        GoldGate,
+        IronGate,
+        HowlingGarden,
+        LostLagoon,
+        MistyMarsh,
+        Observatory,
+        PhantomRock,
+        SilverGate,
+        TempleOfMoon,
+        TempleOfSun,
+        TidalPalace,
+        TwilightHollow,
+        WatchTower,
+        WhisperingGarden
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
