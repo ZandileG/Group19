7119a8750f038715cda13fc3271c2a8269e239ec
Zandile <2562617@students.wits.ac.za>
2023-06-15 20:31:07 +0200

    Edited the game scene a bit

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneUi: MonoBehaviour
+public class MainMenu: MonoBehaviour
 {
     public void Play()
     {
