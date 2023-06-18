ccc3e40e94b2903f1d2f0eefec966e7da312c5ef
CayCall <2565053@students.wits.ac.za>
2023-06-18 12:10:51 +0200

    Worked on Adventure Cards
    Managed to Shuffle all Adventure Cards.
    First 4 get Displayed  according to specific child objects, last 2 get destroyed.

--- Assets/Scripts/Cards/PlayerDeck.cs
+++ Assets/Scripts/Cards/PlayerDeck.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public class PlayerDeck : MonoBehaviour
 {
-  
-  
+    public GameObject[] DeckList;
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,71 +4,96 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    /* public GameObject[] TreasureCards;
-     private int currentPlayerIndex = 0; // Index of the current player
-     private bool gameEnded = false; // Indicates whether the game has ended
-     // Add other variables as needed
+    public GameObject[] TreasureCards;
+    public GameObject[] Players;
+    private void ShuffleTreasureCards()
+    {
+        int cardCount = TreasureCards.Length;
+        if (cardCount >= 6)
+        {
+            for (int i = 0; i < cardCount; i++)
+            {
+                int randomIndex = Random.Range(i, cardCount);
+                GameObject tempCard = TreasureCards[randomIndex];
+                TreasureCards[randomIndex] = TreasureCards[i];
+                TreasureCards[i] = tempCard;
+            }
 
-     private void ShuffleTreasureCards()
-     {
-         int cardCount = TreasureCards.Length;
-         while (cardCount > 1)
-         {
-             cardCount--;
-             int randomIndex = Random.Range(0, cardCount + 1);
-             GameObject tempCard = TreasureCards[randomIndex];
-             TreasureCards[randomIndex] = TreasureCards[cardCount];
-             TreasureCards[cardCount] = tempCard;
-         }
-     }
+            for (int i = cardCount - 1; i >= cardCount - 2; i--)
+            {
+                Destroy(TreasureCards[i]);
+                TreasureCards[i] = null;
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                TreasureCards[i].SetActive(true);
+            }
 
-     public void Start()
-     {
-         ShuffleTreasureCards();
-     }
+            int playerCount = Players.Length;
+            for (int i = 0; i < Mathf.Min(playerCount, 4); i++)
+            {
+                GameObject player = Players[i];
+                if (player != null)
+                {
+                  
+                    Transform cardDisplay = player.transform.GetChild(0);
 
+                    
+                    GameObject card = Instantiate(TreasureCards[i], cardDisplay.position, Quaternion.identity);
+                    card.transform.SetParent(cardDisplay, false);
+                }
+            }
+        }
+    }
 
-     public void StartGame()
-     {
-          // Initialize game state and setup the game board
-         currentPlayerIndex = 0;
-         gameEnded = false;
-         // Perform any other necessary initialization tasks
-     }
-
-     public void EndGame()
-     {
-             // Clean up game state and show the game-over screen
-         gameEnded = true;
-           // Perform any necessary clean-up tasks
-     }
-
-     public void NextTurn()
-     {
-
-             //Extra - Once a turn is done it, ensure currentplayerindex++;
-         if (gameEnded)
-         {
-             Debug.LogWarning("The game has ended. Cannot proceed to the next turn.");
-             return;
-         }
-
-             // Perform actions to end the current player's turn, update game state, and proceed to the next player's turn
-          // For example, you could update the currentPlayerIndex, handle any game events or triggers, etc.
-
-         // Check for win/loss conditions and end the game if necessary
+    public void Start()
+    {
+        ShuffleTreasureCards();
+    }
 
 
-         /*if (CheckForWinCondition())
-         {
-             // Handle win condition
-             EndGame();
-         }
-         else if (CheckForLossCondition())
-         {
-             // Handle loss condition
-             EndGame();
-         }*/
+    /*public void StartGame()   
+    {
+         // Initialize game state and setup the game board
+        currentPlayerIndex = 0;
+        gameEnded = false;
+        // Perform any other necessary initialization tasks
+    }
+
+    public void EndGame()
+    {
+            // Clean up game state and show the game-over screen
+        gameEnded = true;
+          // Perform any necessary clean-up tasks
+    }
+
+    public void NextTurn()
+    {
+
+            //Extra - Once a turn is done it, ensure currentplayerindex++;
+        if (gameEnded)
+        {
+            Debug.LogWarning("The game has ended. Cannot proceed to the next turn.");
+            return;
+        }
+
+            // Perform actions to end the current player's turn, update game state, and proceed to the next player's turn
+         // For example, you could update the currentPlayerIndex, handle any game events or triggers, etc.
+
+        // Check for win/loss conditions and end the game if necessary
+
+
+        /*if (CheckForWinCondition())
+        {
+            // Handle win condition
+            EndGame();
+        }
+        else if (CheckForLossCondition())
+        {
+            // Handle loss condition
+            EndGame();
+        }*/
     /* private bool CheckForWinCondition()
     {
         // Implement the logic to check if the players have won the game
@@ -81,5 +106,5 @@ private bool CheckForLossCondition()
         // Return true if the loss condition is met, otherwise return false
     }*/
 
-//} 
-} 
+    //} 
+}
