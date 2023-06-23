fe7ccfb346b2891fa49191be743c86e20f2865f1
Zandile <2562617@students.wits.ac.za>
2023-06-23 14:26:29 +0200

    TileLayout Group halfway done

---
+++ Assets/Scripts/Actions/Pawn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Pawn : MonoBehaviour/*, IBeginDragHandler, IDragHandler, IEndDragHandler*/
+{
+  /* //The item's image is visible in the Inspector
+    [SerializeField] private Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    //These are also visible in the Inspector
+    public MoveItem tile;
+
+   //This controls what happens when an item is dragged
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+            parentAfterDrag = transform.parent;
+            transform.SetParent(transform.root);
+            transform.SetAsLastSibling();
+            image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }*/
+}
---
+++ Assets/Scripts/Actions/PawnMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*public class PawnMovement : MonoBehaviour, IDropHandler
+{
+    public MovePawn pawn;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Item_Image item_Image = eventData.pointerDrag.GetComponent<Item_Image>();
+
+        //Check if the item's price is less than the available money
+        if (item_Image != null && item_Image.price <= budget.money)
+        {
+            //Check if the item's parent is empty
+            if (transform.childCount == 0)
+            {
+                //The new parent will be the backpack slot after dragging
+                item_Image.parentAfterDrag = transform;
+            }
+        }
+
+        else
+        {
+            //Check if the item is not coming from the Chest
+            if (eventData.pointerDrag.GetComponentInParent<MoveChestItem>() == null)
+            {
+                //Remove the item from the backpack
+                StartCoroutine(RemoveItemFromBag(eventData.pointerDrag, 1f));
+            }
+        }
+    }
+
+    private IEnumerator RemoveItemFromBag(GameObject item_Image, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(item_Image);
+    }
+}*/
