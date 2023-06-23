2d52e593539aac5bd4a140f46adff28085791a02
Aaminah1 <132408023+Aaminah1@users.noreply.github.com>
2023-06-23 16:46:50 +0200

    Player turns
    Added a script responsible for controlling the player turns to the Game Canvas and added  text component  inside the player prefab. For now the turns change based on time (5 seconds), just to see that the code was working, but once the action script are complete then will change this time condition .

---
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TurnManager : MonoBehaviour
+{
+    public GameObject[] players; // array of player prefabs
+    public float turnDuration = 5f; // duration of each turn in seconds
+    private int currentPlayer = 0; // variable to keep track of current player's turn
+
+    void Start()
+    {
+        UpdatePlayerVisibility();
+        StartCoroutine(ChangeTurns());
+    }
+
+    IEnumerator ChangeTurns()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(turnDuration); // wait for turn duration
+            currentPlayer = (currentPlayer + 1) % players.Length; // update current player
+            UpdatePlayerVisibility();
+        }
+    }
+
+    void UpdatePlayerVisibility()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            TextMeshProUGUI turnText = players[i].transform.Find("yourTurnTxt").GetComponent<TextMeshProUGUI>(); // find TextMeshProUGUI element in player prefab with tag "tmpr"
+            if (i == currentPlayer)
+            {
+                turnText.enabled = true; // show current player's text
+            }
+            else
+            {
+                turnText.enabled = false; // hide other player's text
+            }
+        }
+    }
+}
+
+
+
