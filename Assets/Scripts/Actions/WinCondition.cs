e209bed95723b3a79b8fa732c05b7d0bd0a214da
Dylancie21 <2562617@students.wits.ac.za>
2023-06-25 20:54:57 +0200

    LayoutChange

---
+++ Assets/Scripts/Actions/WinCondition.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class WinCondition : MonoBehaviour, IDropHandler
+{
+    public Canvas gameCanvas;
+    public Canvas winOrLoseCanvas;
+    public GameObject winText;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Pawn pawn = eventData.pointerDrag.GetComponent<Pawn>();
+
+    //Check if the pawn's parent is empty
+        if (transform.childCount == 3)
+        {
+          //The new parent after the drag
+            pawn.parentAfterDrag = transform;
+
+            gameCanvas.gameObject.SetActive(false);
+            winOrLoseCanvas.gameObject.SetActive(true);
+            winText.gameObject.SetActive(true);
+        }
+
+        else
+        {
+         //Remove the pawn from the tile
+          StartCoroutine(RemovePawnFromTile(eventData.pointerDrag, 1f));
+        }
+    }
+
+private IEnumerator RemovePawnFromTile(GameObject pawn, float delay)
+    {
+    yield return new WaitForSeconds(delay);
+    Destroy(pawn);
+    }
+}
