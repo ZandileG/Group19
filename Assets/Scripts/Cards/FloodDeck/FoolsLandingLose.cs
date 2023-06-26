e5549cea8ab67c25a23a9f4f2b381a0ee9646da1
Dylancie21 <2562617@students.wits.ac.za>
2023-06-26 23:31:40 +0200

    LoseCondition

--- Assets/Scripts/Cards/FloodDeck/FoolsLandingLose.cs
+++ Assets/Scripts/Cards/FloodDeck/FoolsLandingLose.cs
@@ -2,32 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class FoolsLandingLose : MonoBehaviour
 {
-    /* public GameObject foolsLandingTile; // assign the Fools' Landing tile in the Inspector
-    public string loseSceneName; // assign the name of the Lose scene in the Inspector
-
-    void Update()
-    {
-        if (!foolsLandingTile.activeInHierarchy) // check if the Fools' Landing tile is inactive
-        {
-            // end the game
-            SceneManager.LoadScene(loseSceneName); // load the Lose scene
-        }
-    }*/
-    public GameObject foolsLandingTile; // assign the Fools' Landing tile in the Inspector
+    public GameObject islandTile; //assign the Fools' Landing tile in the Inspector
+    public GameObject floodTile; // assign the Fools' Landing flooded tile in the Inspector
     public GameObject winOrLoseCanvas; // assign the WinOrLoseCanvas in the Inspector
+    public GameObject loseText;
 
-void Start()
+/*void Start()
     {
-        foolsLandingTile = GameObject.Find("Flooded-islandTile_Fool'sLanding"); // find the sunk Fools' Landing tile by its name
-    }
+        floodTile = GameObject.Find("Flooded-islandTile_Fool'sLanding")
+        islandTile = GameObject.Find("islandTile_Fool'sLanding"); 
+    }*/
+
      void Update()
     {
-        if (!foolsLandingTile.activeInHierarchy) // check if the Fools' Landing tile is inactive
+        if (!islandTile.activeInHierarchy && !floodTile.activeInHierarchy) // check if the Fools' Landing tile is inactive
         {
             // end the game
-            winOrLoseCanvas.SetActive(true); // activate the WinOrLoseCanvas
+            winOrLoseCanvas.SetActive(true); // activate the gameCanvas.gameObject.SetActive(false);
+            winOrLoseCanvas.gameObject.SetActive(true);
+            loseText.gameObject.SetActive(true);
         }
     }
 }
