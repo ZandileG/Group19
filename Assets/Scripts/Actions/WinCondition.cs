ff7cca36f5b084680704ac9c62e4160ef0b5ede5
Dylancie21 <2562617@students.wits.ac.za>
2023-06-26 19:47:27 +0200

    Changes

--- Assets/Scripts/Actions/WinCondition.cs
+++ Assets/Scripts/Actions/WinCondition.cs
@@ -9,32 +9,24 @@ public class WinCondition : MonoBehaviour, IDropHandler
     public Canvas gameCanvas;
     public Canvas winOrLoseCanvas;
     public GameObject winText;
+    public GameObject islandTile;
 
     public void OnDrop(PointerEventData eventData)
     {
         Pawn pawn = eventData.pointerDrag.GetComponent<Pawn>();
 
     //Check if the pawn's parent is not empty
-        if (transform.childCount == 2)
+        if (transform.childCount == 1)
         {
-          //The new parent after the drag
-            pawn.parentAfterDrag = transform;
+            if (islandTile.activeSelf)
+            {
+                //The new parent after the drag
+                pawn.parentAfterDrag = transform;
 
-            gameCanvas.gameObject.SetActive(false);
-            winOrLoseCanvas.gameObject.SetActive(true);
-            winText.gameObject.SetActive(true);
+                gameCanvas.gameObject.SetActive(false);
+                winOrLoseCanvas.gameObject.SetActive(true);
+                winText.gameObject.SetActive(true);
+            }
         }
-
-        else
-        {
-         //Remove the pawn from the tile
-          StartCoroutine(RemovePawnFromTile(eventData.pointerDrag, 1f));
-        }
-    }
-
-private IEnumerator RemovePawnFromTile(GameObject pawn, float delay)
-    {
-    yield return new WaitForSeconds(delay);
-    Destroy(pawn);
     }
 }
